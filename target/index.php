bead770bc460f1d47a96b7e001fb8ce8eabfe798
Ange <s4nixd@gmail.com>
2018-11-24 08:19:22 +0100

    Start building a Target

---
+++ target/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en" >
+
+<head>
+  <meta charset="UTF-8">
+  <title>Target Form</title>
+  <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+
+  <form action="" method="POST">
+    <label for="username">Username:</label>
+    <input type="text" id="username" name="username">
+
+    <label for="password">Password:</label>
+    <input type="password" id="password" name="password">
+
+    <input type="submit" name="submit" value="Connexion">
+  </form>
+
+</body>
+
+</html>
